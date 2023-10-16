6e05a8a4bd0652d318a8fd3455e8fda464293b36
silvervinoth9 <147551482+silvervinoth9@users.noreply.github.com>
2023-10-16 16:21:03 -0700

    python#6

---
+++ python_beginning/6_what_needed.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+a = "There should be one-- and preferably only one --obvious way to do it. Although that way may not be obvious at first unless you're Dutch."
+print(a[26: -90] + a[56: -76] + a[115: -14])
