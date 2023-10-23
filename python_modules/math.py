7776fcb6bf91edc954e9687180153a5b19a8bc6d
silvervinoth9 <147551482+silvervinoth9@users.noreply.github.com>
2023-10-23 14:00:12 -0700

    import_modules

---
+++ python_modules/math.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+some_var = "Math is awesome!"
+
+def add(a, b):
+ 
+    return a + b
+
+def sub(a, b):
+    """Subtraction function"""
+    return a - b
+
+def mul(a, b):
+    """Multiplication function"""
+    return a * b
+
+def div(a, b):
+    """Division function"""
+    return int(a / b)
